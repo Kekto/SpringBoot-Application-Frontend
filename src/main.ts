0fcf4be0d57b04e796b267279245ec625ff6cdfd
Kekto <lekto8814@gmail.com>
2024-03-25 11:52:43 +0100

    first setup

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/aura-light-green/theme.css'
+import { createPinia } from 'pinia'
 
-createApp(App).use(router).mount('#app')
+const pinia = createPinia();
+
+createApp(App).use(pinia).use(PrimeVue).use(router).mount('#app')
